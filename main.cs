0f1ac437b211b04edce528d602491173122cdf47
Dimanqe <134610401+Dimanqe@users.noreply.github.com>
2023-06-26 13:29:45 +0000

    Code modified

--- main.cs
+++ main.cs
@@ -3,15 +3,14 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
 
-		var name = "Jane";
-		
-    var age = checked((byte) int.Parse(Console.ReadLine()));
-		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-		
-		Console.Write("What is your favorite day of week? ");
-
-		var day = (DayOfWeek) int.Parse(Console.ReadLine());
-		Console.WriteLine("Your favorite day is {0}", day);
+	Console.Write("Enter your name: ");
+    var name = Console.ReadLine();
+    Console.Write("Enter your age: ");
+    var age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Your name is {name} and age is {age}");
+    Console.Write("Enter your birthdate: ");
+    var birthdate = Console.ReadLine();
+    Console.Write($"Your birthdate is {birthdate}");
 
   }
 }
